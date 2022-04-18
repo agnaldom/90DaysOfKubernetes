312787e4bfadbda96f69d3cfd5b77c8ed10ecb5a
Agnaldo Marinho <nimbus.agnaldo@bitcapital.com.br>
2022-04-18 16:30:08 -0300

    Update the project

--- chapter2/simple-node/server.js
+++ chapter2/simple-node/server.js
@@ -1,10 +1,13 @@
-var express = require('express');
+const express = require('express')
 
-var app = express();
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
-app.listen(3000, function(){
-    console.log('Listening on port 3000!');
-    console.log(' https://localhost:3000');
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+    console.log(` https://localhost:${port}`)
 })
